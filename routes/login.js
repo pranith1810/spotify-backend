78f892964b87997e1540f20b836618502f01dee2
pranith9810 <n.rao@mountblue.tech>
2020-09-26 14:38:54 +0530

    Resolved some eslint errors in login.js and signUp.js routes

--- routes/login.js
+++ routes/login.js
@@ -15,8 +15,8 @@ const router = express.Router();
 router.post('/', [
   validateEmail('email'),
   validatePassword('password'),
+  // eslint-disable-next-line consistent-return
 ], async (req, res, next) => {
-
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     logger.info(JSON.stringify(errors));
@@ -24,23 +24,20 @@ router.post('/', [
   }
 
   try {
-    let data = await getUser(connection, req.body.email);
+    const data = await getUser(connection, req.body.email);
     if (data.length === 0) {
-      logger.info(`User doesn't exists`)
-      res.status(409).json({ msg: `Users doesn't exists` }).end();
-      return;
-    }
-    else {
-      const passwordChecker = await bcrypt.compare(req.body.password, data[0].password)
+      logger.info('User doesn\'t exists');
+      res.status(409).json({ msg: 'Users doesn\'t exists' }).end();
+    } else {
+      const passwordChecker = await bcrypt.compare(req.body.password, data[0].password);
       if (!passwordChecker) {
-        logger.error(`password doesn't match`);
-        res.status(401).json({ msg: `Password doesn't match` });
-        return;
+        logger.error('password doesn\'t match');
+        res.status(401).json({ msg: 'Password doesn\'t match' }).end();
       }
 
       const claim = {
-        id: data[0].id
-      }
+        id: data[0].id,
+      };
 
       jwt.sign(claim, config.secretToken, (err, token) => {
         if (err) {
@@ -49,16 +46,12 @@ router.post('/', [
         } else {
           res.status(200).json({ token }).end();
         }
-
-      })
+      });
     }
-  }
-
-  catch (err) {
+  } catch (err) {
     logger.error(JSON.stringify(err));
     throw err;
   }
-}
-)
+});
 
 module.exports = router;
