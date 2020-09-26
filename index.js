540e229b069943b9fb031015b0653a0aa1f6708b
pranith9810 <n.rao@mountblue.tech>
2020-09-26 13:54:08 +0530

    Added a route for getting user profile details from the database

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('./logger');
 const config = require('./config/config');
 const createDbAndTables = require('./database/dbInitialize');
 const signUp = require('./routes/signUp');
 const login = require('./routes/login');
+const userProfile = require('./routes/userProfile');
 
 const app = express();
-
-app.use(express.json());
 
 createDbAndTables()
   .then(() => {
@@ -15,8 +15,12 @@ createDbAndTables()
       res.status(200).json({ msg: 'Welcome to the back-end of Spotify' }).end();
     });
 
+    app.use(cors());
+    app.use(express.json());
+
     app.use('/signUp', signUp);
     app.use('/login', login);
+    app.use('/user', userProfile);
 
     app.use((req, res, next) => {
       const err = new Error('Page Not Found');
