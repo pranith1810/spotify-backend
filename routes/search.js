5357a9406b2987947064de85f0292241f21ea1e8
pranith9810 <n.rao@mountblue.tech>
2020-09-30 22:17:55 +0530

    Added a search.js route which sends the songs which are matched based on the name of the song

---
+++ routes/search.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-param-reassign */
+const express = require('express');
+const { connection } = require('../database/dbConnect.js');
+const logger = require('../logger.js');
+const auth = require('../middleware/auth');
+const getSearchedSongs = require('../database/getSearchedSongs');
+
+const router = express.Router();
+
+router.post('/', auth, (req, res, next) => {
+  getSearchedSongs(connection, req.body.songName)
+    .then((data) => {
+      const searchedSongs = [];
+      data.forEach((object) => {
+        if (searchedSongs.length === 0) {
+          object = {
+            ...object,
+            artist_name: [object.artist_name],
+          };
+          searchedSongs.push(object);
+        } else {
+          let flag = 0;
+          searchedSongs.forEach((newObject) => {
+            if (object.song_id === newObject.song_id) {
+              flag = 1;
+              newObject.artist_name.push(object.artist_name);
+            }
+          });
+          if (flag === 0) {
+            object = {
+              ...object,
+              artist_name: [object.artist_name],
+            };
+            searchedSongs.push(object);
+          }
+        }
+      });
+      logger.info('Searched songs are sent successfully!');
+      res.status(200).json(searchedSongs).end();
+    })
+    .catch((error) => {
+      logger.error('Error occurred while searching songs from database');
+      next(error);
+    });
+});
+
+module.exports = router;
